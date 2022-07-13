cb15beaf7d039891f6637b50abe49be3186f8497
adamlien329 <adam@novellink.com>
2022-07-13 14:29:25 +0800

    feat(remove ramsey/uuid package require): remove ramsey/uuid package require

--- src/Racklin/PkUuids/PkUuids.php
+++ src/Racklin/PkUuids/PkUuids.php
@@ -2,7 +2,7 @@
 
 namespace Racklin\PkUuids;
 
-use Ramsey\Uuid\Uuid;
+use Illuminate\Support\Str;
 
 /**
  * Trait PkUuids
@@ -20,8 +20,29 @@ trait PkUuids
 
         static::creating(function ($model) {
             if (!$model->getIncrementing() && empty($model->{$model->getKeyName()})) {
-                $model->{$model->getKeyName()} = Uuid::uuid4()->toString();
+                $model->{$model->getKeyName()} = Str::uuid()->toString();
             }
         });
+    }
+
+
+    /**
+     * Get the value indicating whether the IDs are incrementing.
+     *
+     * @return bool
+     */
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    /**
+     * Get the auto-incrementing key type.
+     *
+     * @return string
+     */
+    public function getKeyType()
+    {
+        return 'string';
     }
 }
